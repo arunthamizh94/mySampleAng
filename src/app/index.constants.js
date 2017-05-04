c70163420345a4174de06c8a10742f906a7f44f4
Arunthamizh Azhagiri <arunthamizh@twilightsoftwares.com>
2017-05-04 18:57:05 +0530

    initialize

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('mySampleAng')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
